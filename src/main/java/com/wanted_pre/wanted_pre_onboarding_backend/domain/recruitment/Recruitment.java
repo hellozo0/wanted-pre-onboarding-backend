9e161faa5edb7f7a751762e972eff5aea45dc754
hellozo0 <ahju0011@gmail.com>
2024-08-10 21:33:08 +0900

    [feat] 채용공고 Entity 생성

---
+++ src/main/java/com/wanted_pre/wanted_pre_onboarding_backend/domain/recruitment/Recruitment.java
@@ -0,0 +1,43 @@
+package com.wanted_pre.wanted_pre_onboarding_backend.domain.recruitment;
+
+import com.wanted_pre.wanted_pre_onboarding_backend.domain.BaseTimeEntity;
+import com.wanted_pre.wanted_pre_onboarding_backend.domain.enterprise.Enterprise;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recruitment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String position;
+
+    @NotNull
+    private int reward;
+
+    @NotNull
+    private String detail;
+
+    @NotNull
+    private String skill;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "enterprise_id")
+    @NotNull
+    private Enterprise enterprise;
+}
